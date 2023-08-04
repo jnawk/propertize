43a02da43cb608a054a103ad7f4a9aa1531840aa
Philip Dowie <philip@jnawk.nz>
2023-08-04 23:21:34 +1200

    v0.0.2

--- src/propertize/propertize.py
+++ src/propertize/propertize.py
@@ -3,7 +3,7 @@ import typing
 X = typing.TypeVar("X")
 
 
-def p(prop: typing.Callable[[...], X]) -> X:
+def p(prop: typing.Union[X, typing.Callable[[...], X]]) -> X:
     """
     Silly little type cast for things that are actually properties
     that PyCharm doesn't recognize are properties
